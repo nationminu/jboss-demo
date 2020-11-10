d022267a59ca0506dd47a19c95fa45b637d1ab19
송민우 <ssong@songmin-uui-iMac.local>
2020-11-10 10:24:02 +0900

    UPDATE mvn

--- mvn.sh
+++ mvn.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run -v "${PWD}:/usr/app/myapp" -v "${PWD}/m2/:/root/.m2/" -w /usr/app/myapp maven:3-jdk-8 mvn "$@"
+docker run -v "${PWD}:/usr/app/myapp" -v "${HOME}/.m2/:/root/.m2/" -w /usr/app/myapp maven:3-jdk-8 mvn "$@"
